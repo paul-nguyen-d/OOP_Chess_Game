72e665b5b2d9f1b342a7ec173c5b769ca6b7de6d
Paul Nguyen <duy.paul.1108@gmail.com>
2024-09-10 18:57:15 +1000

    Add files via upload

---
+++ TestChess/TestChessBoard.cs
@@ -0,0 +1,57 @@
+﻿using CUSTOM_PROGRAM.board;
+using CUSTOM_PROGRAM.pieces;
+using NUnit.Framework;
+using SplashKitSDK;
+using System.Collections.Generic;
+
+namespace TestChessEngine
+{
+    internal class TestChessBoard
+    {
+        private ChessBoard _board;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _board = new ChessBoard();
+        }
+
+        [Test]
+        public void AlternatingSquares()
+        {
+            // the squares that have both EVEN row number and EVEN column number are WHITE
+            for (int i = 0; i < _board.Height; i++)
+            {
+                for (int j = 0; j < _board.Width; j++)
+                {
+                    if ((i % 2 == 0) && (j % 2 == 0))
+                        Assert.AreEqual("white", _board.BoardSquares[i, j].Color);
+                }
+            }
+
+        }
+
+        [Test]
+        public void BlackSquare()
+        {
+            Assert.AreEqual("black", _board.BoardSquares[2, 1].Color);
+        }
+
+        [Test]
+        public void MovePiece()
+        {
+            Square startSquare = _board.GetSquare(1, 0);
+            Piece knight = _board.GetPiece(startSquare); 
+
+            Square destSquare = _board.GetSquare(2, 2);
+            Move m = new Move(startSquare, destSquare);
+
+            _board.MovePiece(m);
+
+            // the start square no longer contains a piece
+            Assert.AreEqual(null, _board.GetPiece(startSquare));
+            // the piece in the destination square
+            Assert.AreEqual(knight, _board.GetPiece(destSquare));
+        }
+    }
+}
---
+++ TestChess/TestCommandManager.cs
@@ -0,0 +1,80 @@
+﻿using CUSTOM_PROGRAM.board;
+using CUSTOM_PROGRAM.pieces;
+using NUnit.Framework;
+
+namespace TestChessEngine
+{
+    internal class TestCommandManager
+    {
+        private ChessBoard _board;
+        private CommandManager _commandManager;
+        private Square _startSquare, _destSquare;
+        private Move _move;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _board = new ChessBoard();
+            _commandManager = new CommandManager();
+
+            _startSquare = _board.GetSquare(1, 0);
+            _destSquare = _board.GetSquare(2, 2);
+            _move = new Move(_startSquare, _destSquare);
+
+        }
+
+        [Test]
+        public void ExecuteCommand()
+        { 
+            Piece knight = _board.GetPiece(_startSquare);
+
+            _commandManager.ExecuteCommand(_board, _move);
+
+            // the start square no longer contains a piece
+            Assert.AreEqual(null, _board.GetPiece(_startSquare));
+            // the piece in the destination square
+            Assert.AreEqual(knight, _board.GetPiece(_destSquare));
+        }
+
+        [Test]
+        public void UndoMoveCommand()
+        {
+            Piece knight = _board.GetPiece(_startSquare);
+
+            _commandManager.ExecuteCommand(_board, _move);
+            _commandManager.UndoCommand();
+
+            // the start square has the piece again
+            Assert.AreEqual(knight, _board.GetPiece(_startSquare));
+            // the destination square no longer has the piece
+            Assert.AreEqual(null, _board.GetPiece(_destSquare));
+        }
+
+        [Test]
+        public void RedoMoveCommand()
+        {
+            Piece knight = _board.GetPiece(_startSquare);
+
+            _commandManager.ExecuteCommand(_board, _move);
+            _commandManager.UndoCommand();
+            _commandManager.RedoCommand();
+
+            // the start square no longer contains a piece
+            Assert.AreEqual(null, _board.GetPiece(_startSquare));
+            // the piece in the destination square
+            Assert.AreEqual(knight, _board.GetPiece(_destSquare));
+        }
+
+        [Test]
+        public void UndoAttackCommand()
+        {
+
+        }
+
+        [Test]
+        public void RedoAttackCommand()
+        {
+
+        }
+    }
+}
